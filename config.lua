9e17336982d8b3942b230f1de7f6c47a1caaa24d
GoodNetworkES <goodnetworkes@gmail.com>
2024-06-10 02:55:30 -0500

    Add files via upload

---
+++ config.lua
@@ -0,0 +1,10 @@
+-- config.lua
+
+Config = {}
+
+Config.SaveInterval = 10000 -- Save interval in milliseconds (in this example, 10 seconds)
+
+
+-- That's all, very simple right?
+-- Eso es todo, ¿muy sencillo verdad? 
+-- @GoodNetworkES
